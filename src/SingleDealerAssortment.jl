5a02a6a09f883515518eda79b6243810e8821f44
NicolasCorvol <nicolas.corvol@eleves.enpc.fr>
2025-11-25 10:11:44 +0100

    initialized packages

--- src/SingleDealerAssortment.jl
+++ src/SingleDealerAssortment.jl
@@ -1,5 +1,30 @@
 module SingleDealerAssortment
 
 # Write your package code here.
+using Combinatorics
+using CSV
+using DataFrames
+using Gurobi
+using IterTools
+using JSON
+using JuMP
+using Plots
+using Random
+using Statistics
+using Distributions
+using Flux
+using InferOpt
+using HiGHS
+using DocStringExtensions: TYPEDEF, TYPEDFIELDS, TYPEDSIGNATURES
+using ProgressMeter
+using Revise
+using StatsPlots
+using TensorBoardLogger
+using JLD2
+using Base.Threads
+using NNlib
+using DataStructures
+using OhMyThreads
+
 
 end
