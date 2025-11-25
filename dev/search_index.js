fb5cbc2502e8a6e5b85404b1651428eb5b18d422
Documenter.jl <documenter@juliadocs.github.io>
2025-11-25 08:56:18 +0000

    build based on 8a3bd0c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SingleDealerAssortment","page":"Home","title":"SingleDealerAssortment","text":"Documentation for SingleDealerAssortment.\n\n","category":"section"}]
+}
